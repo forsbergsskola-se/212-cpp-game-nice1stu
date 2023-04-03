f3e007e19ce7b345cb4bdfee6435d5771f8362d2
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-04-04 00:30:53 +0200

    tidy code

--- cppGameProject/cppGameProject/Player.cpp
+++ cppGameProject/cppGameProject/Player.cpp
@@ -4,8 +4,8 @@ Player::Player(int x, int y, const std::string& imagePath) : x(x), y(y), image(i
 
 void Player::render(SDL_Surface* screenSurface)
 {
-    // set the position of the player on the screen
+    
     SDL_Rect destRect = { x, y, 0, 0 };
-    // render the image of the player on the screen
+    
     SDL_BlitSurface(image.getResource(), nullptr, screenSurface, &destRect);
 }
--- cppGameProject/cppGameProject/Player.h
+++ cppGameProject/cppGameProject/Player.h
@@ -5,9 +5,8 @@
 class Player
 {
 private:
-    int x, y; // the position of the player on the screen
-    Image image; // the image representing the player
-public:
-    Player(int x, int y, const std::string& imagePath); // constructor
-    void render(SDL_Surface* screenSurface); // render the player on the screen
+    int x, y;
+    Image image;
+    Player(int x, int y, const std::string& imagePath);
+    void render(SDL_Surface* screenSurface);
 };
