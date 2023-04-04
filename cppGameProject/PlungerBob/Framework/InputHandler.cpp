4bae3b99f37bca4f9c5b254c8329ea42a320a859
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-04-04 20:57:43 +0200

    Input Handler
    Added input handler to retrieve keyboard and mouse states

---
+++ cppGameProject/PlungerBob/Framework/InputHandler.cpp
@@ -0,0 +1,96 @@
+#include "InputHandler.h"
+
+CInputHandler::CInputHandler(void)
+{
+	#ifdef _DEBUG
+		printf("Info: creating input handler\n");
+	#endif
+}
+
+CInputHandler::~CInputHandler(void)
+{
+	#ifdef _DEBUG
+		printf("Info: destroying input handler\n");
+	#endif
+}
+
+void CInputHandler::Update(void)
+{
+	// Process the keyboard state
+
+	const Uint8* pKeyboardState = SDL_GetKeyboardState(nullptr);
+
+	for(uint32_t i = 0; i < NUM_KEYBOARD_KEYS; ++i)
+	{
+		m_Keyboard.PreviousKeyState[i]	= m_Keyboard.CurrentKeyState[i];
+		m_Keyboard.CurrentKeyState[i]	= (pKeyboardState[i] == SDL_TRUE);
+	}
+
+	// Process the mouse state
+
+	int32_t			MouseX		= 0;
+	int32_t			MouseY		= 0;
+	const Uint32	MouseState	= SDL_GetMouseState(&MouseX, &MouseY);
+
+	for(uint32_t i = 0; i < NUM_MOUSE_BUTTONS; ++i)
+	{
+		m_Mouse.PreviousButtonState[i]	= m_Mouse.CurrentButtonState[i];
+		m_Mouse.CurrentButtonState[i]	= ((MouseState & SDL_BUTTON(i)) ? true : false);
+	}
+
+	m_Mouse.Position = CVector2D((float)MouseX, (float)MouseY);
+
+	const CVector2D PrevMousePos	= m_Mouse.Position;
+	const CVector2D NewMousePos		= CVector2D((float)MouseX, (float)MouseY);
+
+	m_Mouse.Position		= NewMousePos;
+	m_Mouse.DeltaMovement	= NewMousePos - PrevMousePos;
+}
+
+bool CInputHandler::KeyPressed(const uint32_t Key) const
+{
+	if(Key > (NUM_KEYBOARD_KEYS - 1))
+		return false;
+
+	return (m_Keyboard.CurrentKeyState[Key] && !m_Keyboard.PreviousKeyState[Key]);
+}
+
+bool CInputHandler::KeyHeld(const uint32_t Key) const
+{
+	if(Key > (NUM_KEYBOARD_KEYS - 1))
+		return false;
+
+	return m_Keyboard.CurrentKeyState[Key];
+}
+
+bool CInputHandler::KeyReleased(const uint32_t Key) const
+{
+	if(Key > (NUM_KEYBOARD_KEYS - 1))
+		return false;
+
+	return (!m_Keyboard.CurrentKeyState[Key] && m_Keyboard.PreviousKeyState[Key]);
+}
+
+bool CInputHandler::MouseButtonPressed(const uint32_t Button) const
+{
+	if(Button > (NUM_MOUSE_BUTTONS - 1))
+		return false;
+
+	return (m_Mouse.CurrentButtonState[Button] && !m_Mouse.PreviousButtonState[Button]);
+}
+
+bool CInputHandler::MouseButtonHeld(const uint32_t Button) const
+{
+	if(Button > (NUM_MOUSE_BUTTONS - 1))
+		return false;
+
+	return m_Mouse.CurrentButtonState[Button];
+}
+
+bool CInputHandler::MouseButtonReleased(const uint32_t Button) const
+{
+	if(Button > (NUM_MOUSE_BUTTONS - 1))
+		return false;
+
+	return (!m_Mouse.CurrentButtonState[Button] && m_Mouse.PreviousButtonState[Button]);
+}
---
+++ cppGameProject/PlungerBob/Framework/InputHandler.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "Singleton.h"
+#include "Vector2D.h"
+
+#include <SDL.h>
+#include <memory>
+
+#define NUM_KEYBOARD_KEYS (SDL_NUM_SCANCODES)
+#define NUM_MOUSE_BUTTONS (8)
+
+class CInputHandler : public CSingleton<CInputHandler>
+{
+	/**
+	* Keyboard state struct
+	*/
+	struct SKeyboard
+	{
+		SKeyboard(void)
+		{
+			memset(CurrentKeyState,		false, sizeof(CurrentKeyState));
+			memset(PreviousKeyState,	false, sizeof(PreviousKeyState));
+		}
+
+		bool CurrentKeyState[NUM_KEYBOARD_KEYS];
+		bool PreviousKeyState[NUM_KEYBOARD_KEYS];
+	};
+
+	/**
+	* Mouse state struct
+	*/
+	struct SMouse
+	{
+		SMouse(void)
+		: Position(CVector2D::Zero)
+		, DeltaMovement(CVector2D::Zero)
+		{
+			memset(CurrentButtonState,	false, sizeof(CurrentButtonState));
+			memset(PreviousButtonState,	false, sizeof(PreviousButtonState));
+		}
+
+		CVector2D	Position;
+		CVector2D	DeltaMovement;
+		bool		CurrentButtonState[NUM_MOUSE_BUTTONS];
+		bool		PreviousButtonState[NUM_MOUSE_BUTTONS];
+	};
+
+public:
+
+	 CInputHandler					(void);
+	~CInputHandler					(void);
+
+	void		Update				(void);
+
+	bool		KeyPressed			(const uint32_t Key) const;
+	bool		KeyHeld				(const uint32_t Key) const;
+	bool		KeyReleased			(const uint32_t Key) const;
+
+	bool		MouseButtonPressed	(const uint32_t Button) const;
+	bool		MouseButtonHeld		(const uint32_t Button) const;
+	bool		MouseButtonReleased	(const uint32_t Button) const;
+
+public:
+
+	CVector2D&	GetMousePosition	(void)	const	{return (CVector2D&)m_Mouse.Position;}
+
+private:
+
+	SKeyboard	m_Keyboard;
+
+	SMouse		m_Mouse;
+
+};
