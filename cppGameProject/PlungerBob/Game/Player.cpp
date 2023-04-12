8da1b169cdb80b4e7af822d1a77df82a50c95ae9
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-04-12 19:19:13 +0200

    Create Player.h & Player.cpp

---
+++ cppGameProject/PlungerBob/Game/Player.cpp
@@ -0,0 +1,26 @@
+#include "Player.h"
+
+CPlayer::CPlayer()
+{
+}
+
+CPlayer::~CPlayer()
+{
+}
+
+bool CPlayer::Create()
+{
+	return false;
+}
+
+void CPlayer::Destroy()
+{
+}
+
+void CPlayer::Render()
+{
+}
+
+void CPlayer::RenderDebug()
+{
+}
---
+++ cppGameProject/PlungerBob/Game/Player.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class CPlayer
+{
+public:
+
+	CPlayer();
+	~CPlayer();
+
+	bool Create();
+	void Destroy();
+
+	void Render();
+	void RenderDebug();
+
+private:
+	
+};
