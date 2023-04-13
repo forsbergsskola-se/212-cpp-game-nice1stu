491355173c42e47c70641b71fc548c65b8eaeb7a
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-04-13 22:46:49 +0200

    Start collision detection, but not quite working

--- cppGameProject/PlungerBob/Game/Player.cpp
+++ cppGameProject/PlungerBob/Game/Player.cpp
@@ -4,6 +4,22 @@
 #include "Framework/TextureFactory.h"
 #include "Framework/InputHandler.h"
 
+static bool QuadVsQuad(const SDL_FRect& rQuad1, const SDL_FRect& rQuad2, SDL_FRect* pIntersection = nullptr)
+{
+	SDL_FRect	Intersection = { 0.0f, 0.0f, 0.0f, 0.0f };
+	const bool	Result = (SDL_IntersectFRect(&rQuad1, &rQuad2, &Intersection) == SDL_TRUE);
+
+	if (pIntersection)
+	{
+		pIntersection->x = Intersection.x;
+		pIntersection->y = Intersection.y;
+		pIntersection->w = Intersection.w;
+		pIntersection->h = Intersection.h;
+	}
+
+	return Result;
+}
+
 CPlayer::CPlayer()
 :m_pTexture (nullptr)
 , m_Position(CVector2D::Zero)
@@ -15,6 +31,8 @@ CPlayer::CPlayer()
 , m_HorizontalDirection(EPLayerState::IDLE)
 , m_VerticalDirection(EPLayerState::IDLE)
 , m_LookDirection(1)
+, m_CollisionQuad({ 0.0f, 0.0f, 0.0f, 0.0f })
+, m_CollisionQuadOffset(CVector2D::Zero)
 {
 }
 
@@ -25,13 +43,18 @@ CPlayer::~CPlayer()
 bool CPlayer::Create()
 {
 	const CVector2D FrameSize = CVector2D(128.0f, 128.0f);
+	const CVector2D PlayerSize = CVector2D(64.0f, 105.0f);
+	
 
 	m_pTexture = CTextureFactory::GetInstance().CreateTexture("Plumber.png");
 	m_pTexture->SetSize(FrameSize);
+
 	m_pTexture->SetTextureCoords(0, FrameSize.x, 0, FrameSize.y);
 
 	m_Position = CRenderDevice::GetInstance().GetWindow()->GetCenter() - (FrameSize * 0.5);
 	m_Velocity = CVector2D(300.0f, 300.0f);
+	m_CollisionQuadOffset = CVector2D(35.0f, 10.0f);
+	m_CollisionQuad = { m_Position.x + m_CollisionQuadOffset.x, m_Position.y + m_CollisionQuadOffset.y, PlayerSize.x, PlayerSize.y };
 
 	m_pPlayerAnimatorIdle = new CAnimator;
 	m_pPlayerAnimatorRunning = new CAnimator;
@@ -145,21 +168,54 @@ void CPlayer::HandleInput()
 	}
 }
 
-void CPlayer::Update(const float Deltatime)
+void CPlayer::Update(const QuadVector& rCollisionQuads, const QuadVector& rToiletteQuads, const QuadVector& rTriggerQuads, const float Deltatime)
 {
 	if (m_pPlayerAnimatorCurrent)
 	{
 		m_pPlayerAnimatorCurrent->Update(Deltatime);
 	}
 
+	if (m_Plumbing)
+	{
+		return;
+	}
 	const CVector2D MoveAmount = m_Speed * Deltatime;
 
-	m_Position.x += MoveAmount.x;
+	if (m_HorizontalDirection != IDLE)
+	{
+		m_Position.x += MoveAmount.x;
 
-	
+		m_CollisionQuad.x = m_Position.x + m_CollisionQuadOffset.x;
 
-	m_Position.y += MoveAmount.y;
+		for (const SDL_FRect& rCollisionQuad : rCollisionQuads)
+		{
+			ResolveYCollision(rCollisionQuad, MoveAmount);
+		}
 
+		for (const SDL_FRect& rCollisionQuad : rToiletteQuads)
+		{
+			ResolveXCollision(rCollisionQuad, MoveAmount);
+		}
+	}
+
+	if (m_VerticalDirection != IDLE)
+	{
+		m_Position.y += MoveAmount.y;
+
+		m_CollisionQuad.y = m_Position.y + m_CollisionQuadOffset.y;
+
+		for (const SDL_FRect& rCollisionQuad : rCollisionQuads)
+		{
+			ResolveYCollision(rCollisionQuad, MoveAmount);
+		}
+
+		for (const SDL_FRect& rCollisionQuad : rToiletteQuads)
+		{
+			ResolveYCollision(rCollisionQuad, MoveAmount);
+		}
+	}
+
+	CheckTriggers(rTriggerQuads);
 }
 
 void CPlayer::Render()
@@ -170,6 +226,14 @@ void CPlayer::Render()
 void CPlayer::RenderDebug()
 {
 
+}
+
+void CPlayer::PlumbingFinished()
+{
+	m_pCurrentAnimation = m_pAnimatorIdle;
+	m_pCurrentAnimation->Reser();
+
+	m_Plumbing = false;
 }
 
 void CPlayer::ActivateIdleAnimation()
@@ -192,4 +256,91 @@ void CPlayer::ActivateRunningAnimation()
 	}
 	const bool Flipped = (m_LookDirection == 0);
 	m_pTexture->SetFlipMethod(Flipped ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);
+}
+
+void CPlayer::CheckTriggers(const QuadVector& rTriggerQuads)
+{
+	if (m_CurrentTRiggerID != 1)
+	{
+		if (!QuadVsQuad(m_CollisionQuad, rTriggerQuads[m_CurrentTRiggerID]))
+			m_CurrentTRiggerID = -1;
+	}
+	else
+	{
+		for (int i = 0; i < rTriggerQuads.size(); ++i)
+		{
+			if (QuadVsQuad(m_CollisionQuad, rTriggerQuads[i]))
+			{
+				m_CurrentTRiggerID = i;
+
+				break;
+			}
+		}
+	}
+}
+
+void CPlayer::ResolveXCollision(const SDL_FRect& rLevelCollisionQuad, const CVector2D& rMoveAmount)
+{
+	// The player is moving to the left
+	if (rMoveAmount.x < 0.0f)
+	{
+		SDL_FRect Intersection = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+		if (QuadVsQuad(m_CollisionQuad, rLevelCollisionQuad, &Intersection))
+		{
+			m_Position.x += Intersection.w;
+
+			m_CollisionQuad.x = m_Position.x + m_CollisionQuadOffset.x;
+
+			m_Speed.x = 0.0f;
+		}
+	}
+
+	// The player is moving to the right
+	else if (rMoveAmount.x > 0.0f)
+	{
+		SDL_FRect Intersection = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+		if (QuadVsQuad(m_CollisionQuad, rLevelCollisionQuad, &Intersection))
+		{
+			m_Position.x -= Intersection.w;
+
+			m_CollisionQuad.x = m_Position.x + m_CollisionQuadOffset.x;
+
+			m_Speed.x = 0.0f;
+		}
+	}
+}
+
+void CPlayer::ResolveYCollision(const SDL_FRect& rLevelCollisionQuad, const CVector2D& rMoveAmount)
+{
+	// The player is moving up
+	if (rMoveAmount.y < 0.0f)
+	{
+		SDL_FRect Intersection = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+		if (QuadVsQuad(m_CollisionQuad, rLevelCollisionQuad, &Intersection))
+		{
+			m_Position.y += Intersection.h;
+
+			m_CollisionQuad.y = m_Position.y + m_CollisionQuadOffset.y;
+
+			m_Speed.y = 0.0f;
+		}
+	}
+
+	// The player is moving down
+	else if (rMoveAmount.y > 0.0f)
+	{
+		SDL_FRect Intersection = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+		if (QuadVsQuad(m_CollisionQuad, rLevelCollisionQuad, &Intersection))
+		{
+			m_Position.y -= Intersection.h;
+
+			m_CollisionQuad.y = m_Position.y + m_CollisionQuadOffset.y;
+
+			m_Speed.y = 0.0f;
+		}
+	}
 }
--- cppGameProject/PlungerBob/Game/Player.h
+++ cppGameProject/PlungerBob/Game/Player.h
@@ -2,9 +2,17 @@
 
 #include "Framework/Texture.h"
 #include "Framework/Animator.h"
+#include "Framework/Vector2D.h"
+#include <functional>
+#include <vector>
 
 class CPlayer
 {
+public:
+
+	typedef std::vector<SDL_FRect> QuadVector;
+	typedef std::function<void(const uint32_t ToiletteID)> PlumbingStartCB;
+
 public:
 
 	CPlayer();
@@ -14,13 +22,25 @@ public:
 	void Destroy();
 
 	void HandleInput();
-	void Update(const float Deltatime);
+	void Update(const QuadVector& rCollisionQuad, const QuadVector& rToiletteQuads, const QuadVector& rTriggerQuads, const float Deltatime);
 
 	void Render();
 	void RenderDebug();
 
+	void PlumbingFinished();
+
+public:
+
+	void SetPlumbingStartCallback(PlumbingStartCB pPlumbingStartCallback)
+	{
+		m_pPlumbingStartCallback = pPlumbingStartCallback;
+	}
+
 private:
 
+	void CheckTriggers(const QuadVector& rTriggerQuads);
+	void ResolveXCollision(const SDL_FRect& LevelCollisionQuad, const CVector2D& MoveAmount);
+	void ResolveYCollision(const SDL_FRect& LevelCollisionQuad, const CVector2D& MoveAmount);
 	void ActivateIdleAnimation();
 	void ActivateRunningAnimation();
 
@@ -30,10 +50,16 @@ private:
 	CVector2D m_Position;
 	CVector2D m_Velocity;
 	CVector2D m_Speed;
+	CVector2D m_CollisionQuadOffset;
 
 	CAnimator* m_pPlayerAnimatorCurrent;
 	CAnimator* m_pPlayerAnimatorIdle;
 	CAnimator* m_pPlayerAnimatorRunning;
+
+	PlumbingStartCB m_pPlumbingStartCallback;
+
+	uint32_t m_CurrentTRiggerID;
+
 
 	enum EPLayerState
 	{
@@ -44,8 +70,11 @@ private:
 		RUNNING_DOWN
 	};
 
-	int m_HorizontalDirection;
-	int m_VerticalDirection;
-	int m_LookDirection;
+	uint32_t m_HorizontalDirection;
+	uint32_t m_VerticalDirection;
+	uint32_t m_LookDirection;
 
+	bool m_Plumbing;
+
+	SDL_FRect m_CollisionQuad;
 };
